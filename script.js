ff64f298b398bbede227b59d9b0d3985d92d36cc
britanyjackson <62608268+britanyjackson@users.noreply.github.com>
2020-04-29 14:46:13 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript Documentfunction scroll
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+ 
